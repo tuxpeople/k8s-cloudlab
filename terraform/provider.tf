04949e5b5436d51ea9b866134ae1095bece3b11e
Thomas Deutsch <thomas@tuxpeople.org>
2022-03-15 12:04:51 +0100

    various changes

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,4 +1,7 @@
 terraform {
+
+  required_version = "~> 1.0"
+
   backend "azurerm" {
     resource_group_name  = "tstate-rg"
     storage_account_name = "tfstate11747"
@@ -11,13 +14,30 @@ terraform {
       source  = "azurerm"
       version = "~>2.0"
     }
-  }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.2"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.10.0"
+    }
 
-  required_version = "~> 1.0"
+  }
 }
 
 provider "azurerm" {
   # The "feature" block is required for AzureRM provider 2.x.
   # If you're using version 1.x, the "features" block is not allowed.
   features {}
+}
+
+provider "kubectl" {}
+
+provider "kubernetes" {
+  host = azurerm_kubernetes_cluster.k8s.kube_config.0.host
+
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.k8s.kube_config.0.client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.k8s.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.k8s.kube_config.0.cluster_ca_certificate)
 }
