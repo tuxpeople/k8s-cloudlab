d45e180b8235290f23908c6747101a50a584dc9a
Thomas Deutsch <thomas@tuxpeople.org>
2022-04-13 17:31:48 +0200

    specify kubernetes version

--- terraform/aks.tf
+++ terraform/aks.tf
@@ -16,6 +16,8 @@ resource "azurerm_kubernetes_cluster" "k8s" {
   location            = azurerm_resource_group.k8s.location
   resource_group_name = azurerm_resource_group.k8s.name
   dns_prefix          = var.dns_prefix
+  // az aks get-versions --location eastus -o table
+  kubernetes_version  = "1.22.6"
 
   linux_profile {
     admin_username = "ubuntu"
