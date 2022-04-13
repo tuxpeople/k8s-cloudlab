e1b5a029ec83a92e0a1b115fa8e59c6900676ec3
Thomas Deutsch <thomas@tuxpeople.org>
2022-04-13 17:09:52 +0200

    adjustments

--- terraform/flux.tf
+++ terraform/flux.tf
@@ -16,5 +16,6 @@ resource "kubernetes_secret" "aks-secrets" {
 
   data = {
     loadBalancerIP = azurerm_public_ip.aks_lb_ingress.ip_address
+    resource_group_name = azurerm_public_ip.aks_lb_ingress.resource_group_name
   }
 }
