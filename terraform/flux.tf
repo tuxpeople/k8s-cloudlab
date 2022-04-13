79810641bd5fc5627c2cb00d2258e50a60614911
Thomas Deutsch <thomas@tuxpeople.org>
2022-04-13 17:24:00 +0200

    fmt

--- terraform/flux.tf
+++ terraform/flux.tf
@@ -15,7 +15,7 @@ resource "kubernetes_secret" "aks-secrets" {
   }
 
   data = {
-    loadBalancerIP = azurerm_public_ip.aks_lb_ingress.ip_address
+    loadBalancerIP      = azurerm_public_ip.aks_lb_ingress.ip_address
     resource_group_name = azurerm_public_ip.aks_lb_ingress.resource_group_name
   }
 }
