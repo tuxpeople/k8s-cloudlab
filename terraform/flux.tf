9034e605f9ba8e895d724b86f3aa1750532da719
Thomas Deutsch <thomas@tuxpeople.org>
2022-04-13 17:02:27 +0200

    change flux

--- terraform/flux.tf
+++ terraform/flux.tf
@@ -1,109 +1,20 @@
-locals {
-  known_hosts = "github.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg="
+data "kustomization_build" "flux_system" {
+  path = "../cluster/base/flux-system"
 }
 
-resource "tls_private_key" "main" {
-  algorithm   = "ECDSA"
-  ecdsa_curve = "P256"
+resource "kustomization_resource" "flux_system" {
+  for_each = data.kustomization_build.flux_system.ids
+
+  manifest = data.kustomization_build.flux_system.manifests[each.value]
 }
 
-# Flux
-data "flux_install" "main" {
-  target_path = var.target_path
-}
-
-data "flux_sync" "main" {
-  target_path = var.target_path
-  url         = "ssh://git@github.com/${var.github_owner}/${var.repository_name}.git"
-  branch      = var.branch
-}
-
-# Kubernetes
-resource "kubernetes_namespace" "flux_system" {
+resource "kubernetes_secret" "aks-secrets" {
   metadata {
-    name = "flux-system"
-  }
-
-  lifecycle {
-    ignore_changes = [
-      metadata[0].labels,
-    ]
-  }
-}
-
-data "kubectl_file_documents" "install" {
-  content = data.flux_install.main.content
-}
-
-data "kubectl_file_documents" "sync" {
-  content = data.flux_sync.main.content
-}
-
-locals {
-  install = [for v in data.kubectl_file_documents.install.documents : {
-    data : yamldecode(v)
-    content : v
-    }
-  ]
-  sync = [for v in data.kubectl_file_documents.sync.documents : {
-    data : yamldecode(v)
-    content : v
-    }
-  ]
-}
-
-resource "kubectl_manifest" "install" {
-  for_each   = { for v in local.install : lower(join("/", compact([v.data.apiVersion, v.data.kind, lookup(v.data.metadata, "namespace", ""), v.data.metadata.name]))) => v.content }
-  depends_on = [kubernetes_namespace.flux_system]
-  yaml_body  = each.value
-}
-
-resource "kubectl_manifest" "sync" {
-  for_each   = { for v in local.sync : lower(join("/", compact([v.data.apiVersion, v.data.kind, lookup(v.data.metadata, "namespace", ""), v.data.metadata.name]))) => v.content }
-  depends_on = [kubernetes_namespace.flux_system]
-  yaml_body  = each.value
-}
-
-resource "kubernetes_secret" "main" {
-  depends_on = [kubectl_manifest.install]
-
-  metadata {
-    name      = data.flux_sync.main.secret
-    namespace = data.flux_sync.main.namespace
+    name      = "aks-secrets"
+    namespace = "flux-system"
   }
 
   data = {
-    identity       = tls_private_key.main.private_key_pem
-    "identity.pub" = tls_private_key.main.public_key_pem
-    known_hosts    = local.known_hosts
+    loadBalancerIP = azurerm_public_ip.aks_lb_ingress.ip_address
   }
-}
-
-# GitHub
-resource "github_repository_deploy_key" "main" {
-  title      = "staging-cluster"
-  repository = var.repository_name
-  key        = tls_private_key.main.public_key_openssh
-  read_only  = true
-}
-
-resource "github_repository_file" "install" {
-  repository = var.repository_name
-  file       = data.flux_install.main.path
-  content    = data.flux_install.main.content
-  branch     = var.branch
-}
-
-resource "github_repository_file" "sync" {
-  repository = var.repository_name
-  file       = data.flux_sync.main.path
-  content    = data.flux_sync.main.content
-  branch     = var.branch
-}
-
-resource "github_repository_file" "kustomize" {
-  repository = var.repository_name
-  file       = data.flux_sync.main.kustomize_path
-  content    = data.flux_sync.main.kustomize_content
-  branch     = var.branch
 }
